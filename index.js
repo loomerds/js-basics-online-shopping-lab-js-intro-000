b4ddc2148f79fc041d41a6f2dcd02ad5da233553
Doug Loomer <loomerds@gmail.com>
2019-05-12 21:54:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,5 +56,5 @@ function placeOrder(cardNumber) {
   } else {
     cart = [];
   }
-  return "Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}."
+  return `Your total cost is $${totalCost}, which will be charged to the card ${cardNumber}.`;
 }
