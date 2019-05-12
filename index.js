cf69fdd14d0157532a7351dffb7a42a05349d15b
Doug Loomer <loomerds@gmail.com>
2019-05-12 21:40:21 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -36,7 +36,17 @@ function total() {
 }
 
 function removeFromCart(item) {
-  // write your code here
+  let removed = false;
+  for(let i=0;  i<cart.length; i++) {
+    if(item===cart[i].itemName) {
+      cart.splice(i,1);
+      removed = true;
+    }
+  }
+  if(removed===true) {
+    return cart;
+  }
+  return "That item is not in your cart.";
 }
 
 function placeOrder(cardNumber) {
